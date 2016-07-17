527d4a29e598c37b6c185217aedd298e6b096f5d
Belonias <iakovosbelonias@gmail.com>
2016-07-17 20:06:05 +0300

    views and home.html update

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
     title = 'Welcome'
+    form = SignUpForm(request.POST or None)
+
     if request.user.is_authenticated():
         title = "Welcome Back %s" %(request.user)
 
-    #add a form
     context = {
     "title": title,
+    "form": form,
 
     }
+
+    # we need a valid form
+    if form.is_valid():
+        instance = form.save(commit=False)
+        full_name = form.cleaned_data.get("full_name")
+        # we still want the email although we don't have full_name
+        if not full_name:
+            full_name = "No Name"
+        instance.full_name = full_name
+        instance.save()
+        context = {
+        "title": "Thank you"
+        }
+
     return render(request, "home.html", context)
